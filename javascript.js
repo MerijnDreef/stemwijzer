f7f2048419e0d57c5774655fa62b7304de4e61c9
Merijn <99054535@mydavinci.nl>
2021-02-11 14:52:34 +0100

    javascript beter ingedeeld html ook
    geprobeerd veel te doen: failed, probeer beter in elkaar te zetten: succes

--- javascript.js
+++ javascript.js
@@ -1,9 +1,24 @@
-var start = document.createElement("button");
-start.innerHTML = "start";
+let currentSubject = 0;
 
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(start);
+const start = document.getElementById("start");
+const eens = document.getElementById("eens");
+const gvb = document.getElementById("gvb");
+const oneens = document.getElementById("oneens");
+const title = document.getElementById("title");
+const statement = document.getElementById("statement");
+const next = document.getElementById("next");
+const previous = document.getElementById("previous");
+const container = document.getElementById("container");
 
 start.addEventListener ("click", function() {
     alert("did something");
+    show(container);
   });
+
+function show(element) {
+  element.classlist.remove("hidden");
+}
+
+function hide(element) {
+  element.classlist.add("hidden");
+}
