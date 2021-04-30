9c30b8f3d9fed78d9e663ea8febe4e03e21280e0
Merijn <99054535@mydavinci.nl>
2021-04-30 12:05:52 +0200

    alright now awaiting validation
    yep it is basically done, so now I need others to validate this

--- javascript.js
+++ javascript.js
@@ -2,15 +2,15 @@ let currentSubject = 0;
 
 const startContainer = document.getElementById("startContainer");
 const start = document.getElementById("start");
-const agree = document.getElementById("eens");
-const neither = document.getElementById("gvb");
-const disagree = document.getElementById("oneens");
-const titel = document.getElementById("titel");
-const statement = document.getElementById("uitspraak");
-const skip = document.getElementById("overslaan");
-const previous = document.getElementById("vorige");
+const agree = document.getElementById("agree");
+const neither = document.getElementById("neither");
+const disagree = document.getElementById("disagree");
+const titel = document.getElementById("title");
+const statement = document.getElementById("statement");
+const skip = document.getElementById("skip");
+const previous = document.getElementById("previous");
 const container = document.getElementById("container");
-const submitPrevious = document.getElementById("submitVorige");
+const submitPrevious = document.getElementById("submitPrevious");
 const submit = document.getElementById("form1");
 const partySubmit = document.getElementById("form2");
 const resultParties = document.getElementById("resultParties");
@@ -30,7 +30,6 @@ start.addEventListener ("click", function() {
 */
 skip.addEventListener ("click", function() {
   fillChoice("");
-  console.log(subjects);
   if ( (subjects.length -1) != currentSubject) {
     //stop
     currentSubject++;
@@ -42,7 +41,6 @@ skip.addEventListener ("click", function() {
   } else {
     checkQuestion();
   }
-  console.log(currentSubject);
 });
 
 /*
@@ -62,7 +60,6 @@ previous.addEventListener ("click", function() {
 
     answerBackground();
   }
-  console.log(currentSubject);
 });
 
 /*
@@ -91,7 +88,6 @@ disagree.addEventListener ("click", function() {
 */
 function fillChoice(insert) {
   choices[currentSubject] = insert;
-  console.log(choices);
 }
 
 /*
@@ -112,34 +108,21 @@ function hide(element) {
 * this is to add points to the right parties who have the same answers but also which important questions you chose
 */
 function checkQuestion() {
-  var counter = 0;
-  for (var i = 0; i < subjects.length; i++){
-    if (choices[i] === "") {
-      counter++;
-    }
-  }
-  if (counter != 0){
-    alert(counter);
-  } else {
-
-     for (var k = 0; k < choices.length; k++) {
-      for (var l = 0; l < subjects[k].parties.length; l++) {  
-       if (choices[k] == subjects[k].parties[l].position) {   
+  for (var k = 0; k < choices.length; k++) {
+    for (var l = 0; l < subjects[k].parties.length; l++) {  
+      if (choices[k] == subjects[k].parties[l].position) {   
         for (var p = 0; p < choicePoints.length; p++) {
           if (choicePoints[p].name == subjects[k].parties[l].name) {
             if (choices[k] != "") {
               choicePoints[p].points++;
-              console.log(choicePoints);
-              if (questImportant[k] != NaN) {
-              choicePoints[p].points++;
-              }
+                if (questImportant[k] == true) {
+                  choicePoints[p].points++;
+                }
             } 
-            console.log(choicePoints);
           }
         }
-       }     
-      }
-     }
+      }     
+    }
   }
 }
 
@@ -153,7 +136,6 @@ function displayResult() {
       // remove the party
     }
   }
-  console.log(choicePoints);
 
   choicePoints.sort((a, b) => b.points - a.points);
 
@@ -173,14 +155,12 @@ function displayImportant() {
   }
 
   for (i = 0; i < subjects.length; i++) {
-    console.log(subjects[i].title);
     var label = document.createElement("label");
     var li = document.createElement("li");
     li.appendChild(label);
 
     var input = document.createElement("input");
     input.setAttribute("type", "checkbox");
-    input.setAttribute("id", i.toString());
     li.appendChild(input);
 
     label.innerHTML = subjects[i].title;
@@ -192,7 +172,6 @@ function displayImportant() {
 * this is to make the checkboxes for the parties but also to make the checkboxes that can check multiple checkboxes
 */
 function displayParties() {
-  // this is for secular
   var secularLabel = document.createElement("label");
   var secularLi = document.createElement("li");
   secularLi.appendChild(secularLabel);
@@ -203,7 +182,6 @@ function displayParties() {
 
   secularInput.addEventListener ("click", function(){
     var secularParties = document.getElementsByClassName("secular");
-    console.log(secularParties);
       for (j = 0; j < secularParties.length; j++) {
         secularParties[j].checked = true
       }
@@ -212,7 +190,6 @@ function displayParties() {
   secularLi.appendChild(secularInput);
   document.getElementById("manyCheck").appendChild(secularLi);
 
-  // this is for big
   var bigLabel = document.createElement("label");
   var bigLi = document.createElement("li");
   bigLi.appendChild(bigLabel);
@@ -223,7 +200,6 @@ function displayParties() {
 
   bigInput.addEventListener ("click", function(){
     var bigParties = document.getElementsByClassName("big");
-    console.log(bigParties);
       for (j = 0; j < bigParties.length; j++) {
         bigParties[j].checked = true
       }
@@ -232,9 +208,7 @@ function displayParties() {
   bigLi.appendChild(bigInput);
   document.getElementById("manyCheck").appendChild(bigLi);
 
-  // this is to make the parties checkbox
   for (i = 0; i < choicePoints.length; i++) {
-    console.log(parties[i].name);
     var label = document.createElement("label");
     var li = document.createElement("li");
     li.appendChild(label);
@@ -277,11 +251,9 @@ function setAnswer(answer) {
     statement.innerHTML = subjects[currentSubject].statement;
     answerBackground();
   } else {
-    // checkQuestion();
     show(submit);
     hide(container);
   }
-  console.log(currentSubject);
 }
 
 /*
@@ -308,19 +280,17 @@ function answerBackground() {
 }
 
 /*
-* this gives the array a number which is basicly useless and could be better, but it sends it to checkQuestion so it can be used right
+* this gives the array a boolean so that the checkQuestion function gets the necessary info it needs
 */
 function checkCheckboxImportant() {
   submit.onsubmit = function(e) {
     e.preventDefault();
-    for (f = 0; f < checkboxes.length; f++) {
-      if (checkboxes[f].checked == true) {
-        id = checkboxes[f].id;
-        var place = parseInt(id, 10);
-        questImportant[f] = place;
-        console.log(place);
-        console.log(questImportant);
-      }
+    for (f = 0; f < questCheckboxes.length; f++) {
+        if (questCheckboxes[f].checked == true) {
+          questImportant[f] = true;
+        } else {
+          questImportant[f] = false;
+        }
     } 
     hide(submit);
     show(partySubmit);
@@ -329,7 +299,7 @@ function checkCheckboxImportant() {
 }
 
 /*
-* this gives the array a boolean so that the displayResult can display it right
+* this gives the array a boolean so that the displayResult can display it right, this is basicly what above this is but different
 */
 function checkCheckboxParty() {
   partySubmit.onsubmit = function(e) {
@@ -337,10 +307,8 @@ function checkCheckboxParty() {
     for (f = 0; f < choicePoints.length; f++){
       if (partyCheckboxes[f].checked == true) {
         chosenParties[f] = true;
-        console.log(chosenParties);
       } else {
         chosenParties[f] = false;
-        console.log(chosenParties);
       }
     } 
     hide(partySubmit);
@@ -349,7 +317,6 @@ function checkCheckboxParty() {
   };
 }
 
-//after the parties are chosen I need to display them from most points to least points
 
 var choicePoints = [];
 
@@ -361,15 +328,13 @@ var chosenParties = [];
 
 displayImportant();
 
-var checkboxes = document.querySelectorAll("#important input");
-console.log(checkboxes);
+var questCheckboxes = document.querySelectorAll("#important input");
 
 checkCheckboxImportant();
 
 displayParties();
 
 var partyCheckboxes = document.querySelectorAll("#allParties input");
-console.log(partyCheckboxes);
 
 checkCheckboxParty();
 
